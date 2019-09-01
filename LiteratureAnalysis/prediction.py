94d7880bc60cfdbe83242fd95f05f856ac22dbc6
clussificate <343260443@qq.com>
2019-09-01 14:22:50 +0800

    Add files via upload

--- LiteratureAnalysis/prediction.py
+++ LiteratureAnalysis/prediction.py
@@ -29,10 +29,17 @@ def preprocess(data, content):
     X_ls_merge = [MergeWord(document) for document in X_ls]
     return X_ls_merge
 
-def save_predictions(data,preds, proba, filename):
+def save_predictions(data,preds, proba, filename, train_DOIs):
+    train_DOIs = train_DOIs.values
     preds = pd.DataFrame(preds,columns=["Sign_Emp","Sign_Exp","Sign_Ana"])
     proba = pd.DataFrame(proba,columns=["Proba_Emp","Proba_Exp","Proba_Ana"])
     merge_data = pd.concat([data,preds,proba],axis =1)
+    merge_data['In_trainset'] = 'No'
+
+    for i, value in enumerate(train_DOIs):
+        print(value)
+        merge_data.loc[merge_data.DOI==value, 'In_trainset'] = 'Yes'
+
     merge_data.to_excel(filename,index = False)
 
 
@@ -53,33 +60,38 @@ if __name__ =="__main__":
     LDA = load_model("model/LDA")
     Tfidf = load_model("model/tfidf")
     ss = load_model("model/ss")
+    train_DOI = load_model("data/Data_0730/trian_DOIs")
 
     # load data
     print("load data......")
-    filename = "data/Data_0730/OR.xlsx"
-    X_data= read_data(filename)
-    print("preprocess data......")
-    X_abs = preprocess(X_data,'N_ABS')
-    X_titkw = preprocess(X_data,['TITLE', 'KEY_WORDS'])
-    # print(X_abs[7])  # test case no.7 data
-    # predict
-    X_abs_TfIdf = Tfidf.transform(X_abs).toarray()
-    X_titkw_cnt = CntVec.transform(X_titkw)
-    X_titkw_lda = LDA.transform(X_titkw_cnt)
+    journals = ["OR","MSOM","MS","POM"]
+    for journal in journals:
+        filename = "data/Data_0730/"+journal+".xlsx"
+        X_data= read_data(filename)
+        print("preprocess %s data......" % filename.split("/")[-1].split('.')[0])
+        X_abs = preprocess(X_data,'N_ABS')
+        X_titkw = preprocess(X_data,['TITLE', 'KEY_WORDS'])
+        # print(X_abs[7])  # test case no.7 data
+        # predict
+        X_abs_TfIdf = Tfidf.transform(X_abs).toarray()
+        X_titkw_cnt = CntVec.transform(X_titkw)
+        X_titkw_lda = LDA.transform(X_titkw_cnt)
 
-    X_merge = merge_features(X_abs_TfIdf, X_titkw_lda)
-    X_std = ss.transform(X_merge)
-    print("make and save predictions....")
-    preds = clf.predict(X_std)
-    proba = clf.predict_proba(X_std)
-    if strategy == 'classifier_chains':
-        preds = preds.toarray()
-        proba = proba.toarray()
+        X_merge = merge_features(X_abs_TfIdf, X_titkw_lda)
+        X_std = ss.transform(X_merge)
+        print("make and save predictions....")
+        preds = clf.predict(X_std)
+        proba = clf.predict_proba(X_std)
+        if strategy == 'classifier_chains':
+            preds = preds.toarray()
+            proba = proba.toarray()
 
-    save_predictions(X_data[['DOI','TITLE','KEY_WORDS','N_ABS']],
-                     preds,proba,filename.split(".")[0]+"_prediction.xlsx")
-    print("draw histograms...")
+        save_predictions(X_data[['DOI','TITLE','KEY_WORDS','N_ABS']],
+                         preds,proba,filename.split(".")[0]+"_prediction.xlsx",train_DOI)
 
-    print("Done!")
+        print("%s done!" %journal)
+
+    print("succeed!")
+
 
 
